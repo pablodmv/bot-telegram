bc4e89ba85da7b56fea6cfd86d4b724fedfb693f
Pablo <pablodmv@gmail.com>
2020-08-24 20:18:01 -0300

    Search text

--- bot.js
+++ bot.js
@@ -8,6 +8,8 @@ const async = require("async");
 let Parser = require("rss-parser");
 let parser = new Parser();
 
+var _globalLastCommand = "";
+
 //Base de datos
 connectDB();
 
@@ -55,15 +57,57 @@ bot.command("/diaria", (ctx) => {
   }
 });
 
+bot.command("/buscar_diaria", (ctx) => {
+  _globalLastCommand = "/buscar_diaria";
+  ctx.reply("Ingrese el texto a buscar");
+});
+
+bot.command("/buscar_observa", (ctx) => {
+  _globalLastCommand = "/buscar_observa";
+  ctx.reply("Ingrese el texto a buscar");
+});
+
 bot.on("text", async (ctx) => {
-  ctx.reply(`Hola ${ctx.chat.first_name}. Para usar el bot ejecuta los siguientes comandos
+  if (
+    _globalLastCommand === "/buscar_diaria" ||
+    _globalLastCommand === "/buscar_observa"
+  ) {
+    let url = "";
+    if (_globalLastCommand === "/buscar_diaria") {
+      url = "https://ladiaria.com.uy/feeds/articulos/";
+    }
+    if (_globalLastCommand === "/buscar_observa") {
+      url = "https://www.elobservador.com.uy/rss/elobservador.xml";
+    }
+    try {
+      let search = ctx.message.text;
+      ctx.reply(`Buscando ${search}`);
+      (async () => {
+        let feed = await parser.parseURL(url);
+        var flag_no_encontro = 0;
+        feed.items.forEach((item) => {
+          if (item.title.search(new RegExp(search, "i")) > 0) {
+            ctx.reply(item.link);
+            flag_no_encontro = 1;
+          }
+        });
+        if (flag_no_encontro === 0) {
+          ctx.reply(`No encontró resultados con ${search}`);
+        }
+      })();
+      _globalLastCommand = "";
+    } catch (error) {
+      ctx.reply(error);
+    }
+  } else {
+    ctx.reply(`Hola ${ctx.chat.first_name}. Para usar el bot ejecuta los siguientes comandos
   /diaria
   /observa`);
-
+    ctx.reply(`También puedes buscar un texto en las noticias. Para buscar ejecuta los siguientes comandos
+  /buscar_diaria  
+  /buscar_observa`);
+  }
   messageDB.postMessage(ctx.from.id, ctx.chat.first_name, ctx.message.text);
-  console.log(ctx.from.id);
-  console.log(ctx.chat.first_name);
-  console.log(ctx.message.text);
 });
 
 //rss_parser();
